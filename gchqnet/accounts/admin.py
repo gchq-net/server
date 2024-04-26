6355aeb48ce8c1f7c3a161ed17389c9a9db8ad63
Dan Trickey <git@trickey.io>
2024-04-26 17:58:09 +0100

    Tweak user model to store less info

--- gchqnet/accounts/admin.py
+++ gchqnet/accounts/admin.py
@@ -7,22 +7,37 @@ from .models import User
 
 
 class UserAdmin(DjangoUserAdmin):
+    list_display = ("username", "email", "display_name", "is_superuser")
+    list_filter = ("is_superuser", "is_active", "groups")
+    search_fields = ("username", "display_name", "email")
+    readonly_fields = ("last_login", "date_joined")
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Personal info"), {"fields": ("display_name", "email")}),
         (
             _("Permissions"),
             {
                 "fields": (
                     "is_active",
-                    "is_staff",
                     "is_superuser",
                 ),
             },
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "display_name", "password1", "password2"),
+            },
+        ),
+    )
 
 
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+admin.site.site_title = "GCHQ.NET Admin"
+admin.site.site_header = "Great Camp Hexpansion Quest"
+admin.site.index_title = "Admin Panel"
--- gchqnet/accounts/models.py
+++ gchqnet/accounts/models.py
@@ -1,5 +1,105 @@
-from django.contrib.auth.models import AbstractUser
+from __future__ import annotations
+
+from typing import Any, TypeVar
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import UserManager as BaseUserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.mail import send_mail
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+_T = TypeVar("_T", bound=models.Model)
 
 
-class User(AbstractUser):
-    pass
+class UserManager(BaseUserManager[_T]):
+    """
+    Custom UserManager to handle is_staff not existing.
+
+    The following functions are called from other parts of Django, e.g ./manage.py
+    """
+
+    def create_user(
+        self, username: str, email: str | None = None, password: str | None = None, **extra_fields: Any
+    ) -> _T:
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)  # type: ignore[attr-defined]
+
+    def create_superuser(
+        self, username: str, email: str | None = None, password: str | None = None, **extra_fields: Any
+    ) -> _T:
+        extra_fields.setdefault("is_superuser", True)
+
+        if not email:
+            raise ValueError("Superuser must have an email.")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)  # type: ignore[attr-defined]
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_("Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    display_name = models.CharField(_("display name"), max_length=30, unique=True)
+
+    email = models.EmailField(_("email address"), blank=True)
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. " "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "display_name"]
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        constraints = [
+            models.CheckConstraint(
+                check=~models.Q(is_superuser=True, email=""),
+                name="superuser_must_have_email",
+                violation_error_message="Any user with superuser permissions must have an email address",
+            )
+        ]
+
+    @property
+    def is_staff(self) -> bool:
+        return self.is_superuser
+
+    def clean(self) -> None:
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+        # if self.is_superuser and not self.email:
+        #     raise ValidationError("Superusers must provide an email address")
+
+    def get_full_name(self) -> str:
+        return self.display_name
+
+    def get_short_name(self) -> str:
+        """Return the short name for the user."""
+        return self.display_name
+
+    def email_user(self, subject: str, message: str, from_email: str | None = None, **kwargs: Any) -> None:
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
