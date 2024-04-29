adf5fee1b0937625c7a2ef2de270b2897c3a0552
Dan Trickey <git@trickey.io>
2024-04-29 19:19:43 +0100

    Add model and admin views for Badge

--- gchqnet/accounts/admin.py
+++ gchqnet/accounts/admin.py
@@ -1,12 +1,54 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.contrib.auth.models import Group
+from django.db.models.query import QuerySet
+from django.forms import ModelForm
+from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
 
-from .models import User
+from .models import Badge, User
+
+
+class BadgeAdminForm(ModelForm):
+    model = Badge
+
+    def clean_mac_address(self) -> str:
+        return self.cleaned_data["mac_address"].upper().replace(":", "-")
+
+
+class BadgeAdminMixin(admin.options.BaseModelAdmin):
+    form = BadgeAdminForm
+    readonly_fields = ("id", "created_at", "updated_at")
+
+    def has_delete_permission(self, request: HttpRequest, obj: Badge | None = None) -> bool:
+        return False
+
+
+class BadgeInlineAdmin(BadgeAdminMixin, admin.StackedInline):
+    model = Badge
+    extra = 0
+
+
+class BadgeAdmin(BadgeAdminMixin, admin.ModelAdmin):
+    model = Badge
+    list_display = ("mac_address", "user_display_name", "user_username", "is_enabled")
+    list_filter = ("is_enabled",)
+    search_fields = ("mac_address", "user__display_name", "user__username")
+
+    @admin.display(ordering="user__display_name", description="User Display Name")
+    def user_display_name(self, badge: Badge) -> str:
+        return badge.user.display_name
+
+    @admin.display(ordering="user__username", description="Username")
+    def user_username(self, badge: Badge) -> str:
+        return badge.user.username
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Badge]:
+        return super().get_queryset(request).select_related("user")
 
 
 class UserAdmin(DjangoUserAdmin):
+    inlines = (BadgeInlineAdmin,)
     list_display = ("username", "email", "display_name", "is_superuser")
     list_filter = ("is_superuser", "is_active", "groups")
     search_fields = ("username", "display_name", "email")
@@ -37,6 +79,7 @@ class UserAdmin(DjangoUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Badge, BadgeAdmin)
 admin.site.unregister(Group)
 admin.site.site_title = "GCHQ.NET Admin"
 admin.site.site_header = "Great Camp Hexpansion Quest"
