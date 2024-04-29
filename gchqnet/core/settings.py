db8ea33cdc2aed0cfeeccd3ad49fdea747d339e2
Dan Trickey <git@trickey.io>
2024-04-29 19:33:48 +0100

    Add quest app

--- gchqnet/core/settings.py
+++ gchqnet/core/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS: list[str] = []
 INSTALLED_APPS = [
     "gchqnet.accounts",
     "gchqnet.hexpansion",
+    "gchqnet.quest",
     "crispy_forms",
     "crispy_forms_gds",
     "django.contrib.admin",
