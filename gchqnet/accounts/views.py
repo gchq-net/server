51d28fad1751d576417b0f659ed93d4bc5058293
Mohit Gupta <git@skybound.link>
2024-05-09 01:30:09 +0100

    some work done based of GH and discord comments

--- gchqnet/accounts/views.py
+++ gchqnet/accounts/views.py
@@ -40,7 +40,7 @@ class MyFindsView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         assert self.request.user.is_authenticated
         locations = Location.objects.all()
-        captures = self.request.user.capture_events.all()
+        captures = self.request.user.capture_events.select_related("location")
 
         def find_capture(location: Location) -> CaptureEvent | None:
             try:
