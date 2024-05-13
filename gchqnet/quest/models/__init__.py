222390f1774dd6837af43cff14c5192128811edc
Dan Trickey <git@trickey.io>
2024-05-13 21:30:19 +0100

    Split up quest models

---
+++ gchqnet/quest/models/__init__.py
@@ -0,0 +1,12 @@
+from .captures import CaptureEvent, RawCaptureEvent
+from .leaderboard import Leaderboard
+from .location import Coordinates, Location, LocationDifficulty
+
+__all__ = [
+    "CaptureEvent",
+    "Coordinates",
+    "Leaderboard",
+    "Location",
+    "LocationDifficulty",
+    "RawCaptureEvent",
+]
