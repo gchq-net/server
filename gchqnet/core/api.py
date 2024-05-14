294104c5d5cb24114a86deaaaccbf586396c650d
Dan Trickey <git@trickey.io>
2024-05-14 19:26:07 +0100

    Drop quest/ from API urls

--- gchqnet/core/api.py
+++ gchqnet/core/api.py
@@ -21,14 +21,14 @@ class MyAuthenticationScheme(OpenApiAuthenticationExtension):  # type: ignore[no
 
 
 router = routers.DefaultRouter()
-router.register("quest/scoreboards", PrivateScoreboardAPIViewset, basename="quest_private_scoreboards")
+router.register("scoreboards", PrivateScoreboardAPIViewset, basename="quest_private_scoreboards")
 
 app_name = "api"
 
 urlpatterns = [
     path("users/me/", profile, name="users_me"),
-    path("quest/scoreboards/global/", GlobalScoreboardAPIView.as_view(), name="quest_global_scoreboard"),
-    path("quest/locations/my-finds/", my_finds_geojson, name="quest_finds_geo"),
+    path("scoreboards/global/", GlobalScoreboardAPIView.as_view(), name="quest_global_scoreboard"),
+    path("locations/my-finds/", my_finds_geojson, name="quest_finds_geo"),
     path("openapi.json", SpectacularJSONAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="api:schema"), name="docs"),
 ] + router.urls
