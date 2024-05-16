c9ac9a3f2fcb18f4d0fdfcd36663ca8aea4c2eaa
Dan Trickey <git@trickey.io>
2024-05-16 18:55:01 +0100

    Add error pages

--- gchqnet/content/urls.py
+++ gchqnet/content/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -7,4 +8,8 @@ app_name = "content"
 urlpatterns = [
     path("about/", views.AboutPage.as_view(), name="about"),
     path("contact/", views.ContactPage.as_view(), name="contact"),
+    path("400/", TemplateView.as_view(template_name="400.html")),
+    path("403/", TemplateView.as_view(template_name="403.html")),
+    path("404/", TemplateView.as_view(template_name="404.html")),
+    path("500/", TemplateView.as_view(template_name="500.html")),
 ]
