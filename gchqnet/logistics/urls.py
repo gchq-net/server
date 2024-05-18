68722c37b99cc31b92ea6aebd722a7dee5a6cbd9
Dan Trickey <git@trickey.io>
2024-05-18 18:40:35 +0100

    Add view to list planned locations

--- gchqnet/logistics/urls.py
+++ gchqnet/logistics/urls.py
@@ -6,4 +6,5 @@ app_name = "logistics"
 
 urlpatterns = [
     path("", views.LogisticsHomeView.as_view(), name="home"),
+    path("planned-locations/", views.PlannedLocationsListView.as_view(), name="planned_list"),
 ]
--- gchqnet/logistics/views.py
+++ gchqnet/logistics/views.py
@@ -1,7 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from gchqnet.logistics.models import PlannedLocation
 
 from .mixins import AllowedLogisticsAccessMixin
 
 
 class LogisticsHomeView(AllowedLogisticsAccessMixin, TemplateView):
     template_name = "pages/logistics/home.html"
+
+
+class PlannedLocationsListView(AllowedLogisticsAccessMixin, ListView):
+    template_name = "pages/logistics/planned_locations/list.html"
+    paginate_by = 15
+    model = PlannedLocation
