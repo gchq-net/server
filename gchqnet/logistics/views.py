92e04a4c959b94575423e2c0d9b8eade67a27890
Dan Trickey <git@trickey.io>
2024-05-19 13:28:19 +0100

    Add breadcrumbs for logistics

--- gchqnet/logistics/views.py
+++ gchqnet/logistics/views.py
@@ -3,22 +3,25 @@ from typing import Any
 from django.contrib import messages
 from django.db import models
 from django.http import HttpResponse
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, TemplateView, UpdateView
 
+from gchqnet.core.mixins import BreadcrumbsMixin
 from gchqnet.logistics.forms import PlannedLocationCreateForm, PlannedLocationDeleteForm, PlannedLocationEditForm
 from gchqnet.logistics.models import PlannedLocation
 
 from .mixins import AllowedLogisticsAccessMixin
 
 
-class LogisticsHomeView(AllowedLogisticsAccessMixin, TemplateView):
+class LogisticsHomeView(AllowedLogisticsAccessMixin, BreadcrumbsMixin, TemplateView):
     template_name = "pages/logistics/home.html"
+    breadcrumbs = [(None, "Logistics Admin")]
 
 
-class PlannedLocationsListView(AllowedLogisticsAccessMixin, ListView):
+class PlannedLocationsListView(AllowedLogisticsAccessMixin, BreadcrumbsMixin, ListView):
     template_name = "pages/logistics/planned_locations/list.html"
     paginate_by = 15
+    breadcrumbs = [(reverse_lazy("logistics:home"), "Logistics Admin"), (None, "Planned Locations")]
 
     def get_search_query(self) -> str:
         query = self.request.GET.get("search", "")
@@ -40,10 +43,15 @@ class PlannedLocationsListView(AllowedLogisticsAccessMixin, ListView):
         )
 
 
-class PlannedLocationCreateView(AllowedLogisticsAccessMixin, CreateView):
+class PlannedLocationCreateView(AllowedLogisticsAccessMixin, BreadcrumbsMixin, CreateView):
     template_name = "pages/logistics/planned_locations/create.html"
     model = PlannedLocation
     form_class = PlannedLocationCreateForm
+    breadcrumbs = [
+        (reverse_lazy("logistics:home"), "Logistics Admin"),
+        (reverse_lazy("logistics:planned_list"), "Planned Locations"),
+        (None, "Create Planned Location"),
+    ]
 
     def get_form_kwargs(self) -> dict[str, Any]:
         kwargs = super().get_form_kwargs()
@@ -59,19 +67,37 @@ class PlannedLocationCreateView(AllowedLogisticsAccessMixin, CreateView):
         return reverse("logistics:planned_edit", args=[self.object.id])
 
 
-class PlannedLocationEditView(AllowedLogisticsAccessMixin, UpdateView):
+class PlannedLocationEditView(AllowedLogisticsAccessMixin, BreadcrumbsMixin, UpdateView):
     template_name = "pages/logistics/planned_locations/edit.html"
     model = PlannedLocation
     form_class = PlannedLocationEditForm
+    breadcrumbs = [
+        (reverse_lazy("logistics:home"), "Logistics Admin"),
+        (reverse_lazy("logistics:planned_list"), "Planned Locations"),
+    ]
+
+    def get_breadcrumbs(self) -> list[tuple[str, str]]:
+        return super().get_breadcrumbs() + [(None, self.object.internal_name)]
 
     def get_success_url(self) -> str:
         return reverse("logistics:planned_edit", args=[self.object.id])
 
 
-class PlannedLocationDeleteView(AllowedLogisticsAccessMixin, DeleteView):  # type: ignore[misc]
+class PlannedLocationDeleteView(AllowedLogisticsAccessMixin, BreadcrumbsMixin, DeleteView):  # type: ignore[misc]
     template_name = "pages/logistics/planned_locations/delete.html"
     model = PlannedLocation
     form_class = PlannedLocationDeleteForm
+
+    breadcrumbs = [
+        (reverse_lazy("logistics:home"), "Logistics Admin"),
+        (reverse_lazy("logistics:planned_list"), "Planned Locations"),
+    ]
+
+    def get_breadcrumbs(self) -> list[tuple[str, str]]:
+        return super().get_breadcrumbs() + [
+            (reverse_lazy("logistics:planned_edit", args=[self.object.id]), self.object.internal_name),
+            (None, "Delete"),
+        ]
 
     def form_valid(self, form: PlannedLocationDeleteForm) -> HttpResponse:
         messages.info(self.request, "Successfully deleted.")
