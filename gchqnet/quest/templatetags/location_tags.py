dd586868c06f5336e4e1a5462f60016fdb837cb2
Dan Trickey <git@trickey.io>
2024-05-23 17:16:17 +0100

    Add colours to difficulty tags

--- gchqnet/quest/templatetags/location_tags.py
+++ gchqnet/quest/templatetags/location_tags.py
@@ -5,6 +5,17 @@ from gchqnet.quest.models import LocationDifficulty
 register = template.Library()
 
 
-@register.filter
-def difficulty_label(difficulty: int) -> str:
-    return LocationDifficulty(difficulty).label
+@register.inclusion_tag("components/govuk/tag.html")
+def difficulty_tag(difficulty: int) -> dict[str, str]:
+    lut = {
+        LocationDifficulty.EASY.value: "blue",
+        LocationDifficulty.MEDIUM.value: "purple",
+        LocationDifficulty.HARD.value: "red",
+        LocationDifficulty.INSANE.value: "orange",
+        LocationDifficulty.IMPOSSIBLE.value: "green",
+    }
+    colour = lut[difficulty]
+    return {
+        "content": LocationDifficulty(difficulty).label,
+        "colour": colour,
+    }
