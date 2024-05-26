7d289b2c92d21c07626c599b475fbec3a09f8416
Dan Trickey <git@trickey.io>
2024-05-26 22:32:25 +0100

    Do not allow creation of internal basic achievements

--- gchqnet/achievements/forms.py
+++ gchqnet/achievements/forms.py
@@ -4,13 +4,13 @@ from crispy_forms_gds.helper import FormHelper
 from crispy_forms_gds.layout import (
     Submit,
 )
-from django.forms import ModelForm
+from django import forms
 
 from gchqnet.accounts.models.user import User
-from gchqnet.achievements.models import BasicAchievement
+from gchqnet.achievements.models import BasicAchievement, BasicAchievementAwardType
 
 
-class BasicAchievementCreateForm(ModelForm):
+class BasicAchievementCreateForm(forms.ModelForm):
     class Meta:
         model = BasicAchievement
         fields = ("display_name", "difficulty", "award_type")
@@ -18,6 +18,9 @@ class BasicAchievementCreateForm(ModelForm):
     def __init__(self, *args: Any, user: User, **kwargs: Any) -> None:
         self.user = user
         super().__init__(*args, **kwargs)
+
+        self.fields["award_type"].choices = [x for x in BasicAchievementAwardType.choices if x[0] != "internal"]  # type: ignore[attr-defined]
+
         self.helper = FormHelper()
         self.helper.add_input(Submit("submit", "Create"))
 
