0976de61d09c6999c4f4e325f7d43376150df232
Dan Trickey <git@trickey.io>
2024-05-26 12:44:50 +0100

    Add cryptography code

---
+++ gchqnet/hexpansion/crypto.py
@@ -0,0 +1,54 @@
+import hashlib
+
+
+def generate_diversified_key(
+    chip_serial_number: bytes,
+    root_key: bytearray | list[int],
+    target_slot: int,
+) -> bytes:
+    """Generates the diversified key for a given device"""
+    serial_pad = [0x00] * (32 - 9)
+
+    generation_hash_data = list(root_key)
+    generation_hash_data += [0x1C, 0x04, target_slot & 0xFF, (target_slot >> 8) & 0xFF]
+    generation_hash_data += [0xEE, 0x01, 0x23]
+    generation_hash_data += [0x00] * 25
+    generation_hash_data += list(chip_serial_number) + serial_pad
+
+    diversified_key = hashlib.sha256(bytes(generation_hash_data)).digest()
+
+    return diversified_key
+
+
+def badge_response_calculation(
+    atsha_serial: bytes,
+    atsha_random: bytes,
+    badge_mac: bytearray | list[int],
+    master_key: bytearray | list[int],
+    slot: int = 0x00,
+) -> bytes:
+    """verifies a response from a badge"""
+
+    # generate diversified key used on the badge
+    marker_key = generate_diversified_key(atsha_serial, master_key, 0x00)
+
+    formatted_mac = bytearray(
+        f"{badge_mac[0]:02X}-{badge_mac[1]:02X}-{badge_mac[2]:02X}-{badge_mac[3]:02X}-{badge_mac[4]:02X}-{badge_mac[5]:02X}",
+        "ascii",
+    )
+
+    challenge = list(formatted_mac) + [0x00] * 3
+
+    # generate tempkey after nonce command
+    noncedata = list(atsha_random) + list(challenge) + [0x16, 0x01, 0x00]
+    atsha_tempkey = hashlib.sha256(bytes(noncedata)).digest()
+
+    # generate expected result from chip
+    otherdata = [0x08, 0x01, slot, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+    # perform a local mac command
+    macdata = list(marker_key) + list(atsha_tempkey)
+    macdata += otherdata[0:4] + [0x00] * 8 + otherdata[4:7] + [0xEE] + otherdata[7:11] + [0x01, 0x23] + otherdata[11:13]
+    client_resp = hashlib.sha256(bytes(macdata)).digest()
+
+    return client_resp
---
+++ gchqnet/hexpansion/tests/test_crypto.py
@@ -0,0 +1,44 @@
+from django.utils.crypto import constant_time_compare
+
+from gchqnet.hexpansion.crypto import badge_response_calculation, generate_diversified_key
+
+
+def test_generate_diversified_key() -> None:
+    key = generate_diversified_key(
+        b"a" * 64,
+        [0x04] * 32,
+        0,
+    )
+    assert key == b"m\x1b:\x83i\xde2iE\xe6\xf7\x02\xd4/\x0c4VH\x91\xc7\xa15\n-\\\x12'\x96\xd7\xeb\xc2S"
+
+
+def test_badge_response_calculation__good() -> None:
+    # check against a set of data provided by a real badge
+    badge_mac = [0xDC, 0x54, 0x75, 0xD8, 0x6E, 0x88]
+
+    serial = b"\x01#]\xc2Q-\xb7a\xee"
+    random = b"N\xab\x86\xb4\xfc\xe89`\\\xb5\xe0\x9f\xb8H`\xdbN_\xe3g\x81\x86\xff\x17\xfc\x88\xb0.\xea\xf4#\xcb"
+    response = (
+        b"b\x84\xa1\xf1E\xdb\xde\xdb\xc6\xf7\x92\xe1\xed*\xc1\x81E\xc1\xe2O\x81_\xf0\xe6_\xa9\x06\x00\x8b_\xe4\xbb"
+    )
+
+    key_0 = bytearray.fromhex("8" * 64)
+
+    expected = badge_response_calculation(serial, random, badge_mac, key_0)
+
+    assert constant_time_compare(response, expected)
+
+
+def test_badge_response_calculation__bad() -> None:
+    # check against a set of data provided by a real badge
+    badge_mac = [0xDC, 0x54, 0x75, 0xD8, 0x6E, 0x88]
+
+    serial = b"\x01#]\xc2Q-\xb7a\xee"
+    random = b"N\xab\x86\xb4\xfc\xe89`\\\xb5\xe0\x9f\xb8H`\xdbN_\xe3g\x81\x86\xff\x17\xfc\x88\xb0.\xea\xf4#\xcb"
+    response = b"a" * 64
+
+    key_0 = bytearray.fromhex("8" * 64)
+
+    expected = badge_response_calculation(serial, random, badge_mac, key_0)
+
+    assert not constant_time_compare(response, expected)
